8f577b3967039959cb03aecaf0c20a801a97d782
jdmedlock <jdmedlock@gmail.com>
2018-09-02 06:30:46 -0500

    Feature: Change href to button
    Change href to button to maintain semantic meaning
    
    Resolves: N/a
    See also: #13

--- js/main.js
+++ js/main.js
@@ -163,9 +163,11 @@ createRestaurantHTML = (restaurant) => {
   address.innerHTML = restaurant.address;
   li.append(address);
 
-  const more = document.createElement('a');
+  const more = document.createElement('button');
   more.innerHTML = 'View Details';
-  more.href = DBHelper.urlForRestaurant(restaurant);
+  more.onclick = (event) => {
+    window.location = DBHelper.urlForRestaurant(restaurant);
+  };
   li.append(more);
 
   return li;
