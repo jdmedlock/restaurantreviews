d0826c6dfa0ad565a204108fd32595239e77dd50
jdmedlock <jdmedlock@gmail.com>
2018-09-02 09:49:41 -0500

    Feature: Add Service Worker logic
    Add Service Worker Logic
    
    Resolves: N/a
    See also: #14

--- js/main.js
+++ js/main.js
@@ -4,6 +4,19 @@ let restaurants,
 var newMap
 var markers = []
 
+// Register a ServiceWorker if supported by the browser
+console.log('GOT HERE');
+if ('serviceworker' in navigator) {
+  navigator.serviceWorker
+    .register('/js/sw.js')
+    .then((reg) => {
+      console.log('ServiceWorker registration succeeded - ', reg.scope);
+    })
+    .catch((error) => {
+      console.log('ServiceWorker registration failed - ', error);
+    });
+}
+
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
